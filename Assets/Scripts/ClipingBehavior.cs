9c862ead363018b034b79446a449009b38359b93
NoahPM <noahplourdem@hotmail.com>
2022-10-07 18:18:51 -0400

    Functionnal clip walls

--- Assets/Scripts/ClipingBehavior.cs
+++ Assets/Scripts/ClipingBehavior.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class ClipingBehavior : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other) {
-        if(other.tag == "Player"){
-            gameObject.GetComponentInParent<BoxCollider>().enabled = false;
+    void OnTriggerEnter(Collider other)  
+    {
+        if(other.gameObject.tag == "Player"){
+            gameObject.transform.parent.gameObject.GetComponent<BoxCollider>().enabled = false;
         }
     }
-    void OnTriggerExit(Collider other) {
-        if(other.tag == "Player"){
-            gameObject.GetComponentInParent<BoxCollider>().enabled = true;
+
+    void OnTriggerExit(Collider other)  
+    {
+        if(other.gameObject.tag == "Player"){
+            gameObject.transform.parent.gameObject.GetComponent<BoxCollider>().enabled = true;
         }
     }
 }

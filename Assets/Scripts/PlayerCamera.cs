136ff6bd08246b6620e9c16ba4f61fc868dd80c8
unknown <maxime.lepage4@uqac.ca>
2022-10-07 17:50:37 -0400

    Adding the First person view character

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    private float xRotation;
+    private float yRotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        // To don't go too far with the camera
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
